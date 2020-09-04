dc31a9806b292d80840196710b376680d41e1d21
Simon <german.marin@mail.escuelaing.edu.co>
2020-09-04 14:45:17 -0500

    repo

--- src/main/java/edu/escuelaing/arep/sparkwebapp/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/sparkwebapp/SparkWebApp.java
@@ -43,8 +43,7 @@ public class SparkWebApp {
                 + "  <br>"
                 + "  <br>"
                 + "  <input type=\"submit\" value=\"Submit\">"
-                + "</form>"
-                + "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>"
+                + "</form>"                
                 + "</body>"
                 + "</html>";
         return pageContent;
